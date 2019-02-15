81fe30eb6be9f73b93afef016242e472a7b610b7
tonikautto <toni@kautto.se>
2019-02-15 17:26:10 +1100

    Updated set-qlik-license.ps1

--- set-qlik-license.ps1
+++ set-qlik-license.ps1
@@ -20,6 +20,40 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+<#
+.SYNOPSIS 
+    Applies license to Qlik Sense site. 
+.DESCRIPTION
+    Applies Qlik Sense site license through Qlik Sense Repository Service (QRS) API, by utilizing Qlik CLI (https://github.com/ahaydon/Qlik-Cli).
+.PARAMETER  LicenseKey
+    16 digit Qlik product license key
+.PARAMETER  ControlNumber
+    5 digit Qlik product license control number
+.PARAMETER  LicenseeName
+    Name of the licensee
+.PARAMETER  LicenseeOrg
+    Name of licensee organisation
+.INPUTS
+    N/A
+.OUTPUTS
+    N/A
+.NOTES
+    Distributed under MIT license. 
+    https://github.com/qliksupport/qlik-cli-examples/LICENSE
+.LINK
+    https://github.com/qliksupport/qlik-cli-examples
+.COMPONENT
+    This script depends on Qlik CLI for PowerShell
+    https://github.com/ahaydon/Qlik-Cli
+.EXAMPLE
+    set-qlik-license.ps1 
+    Call the cmdlet without parameters, and it will prompt for input values.
+.EXAMPLE
+    set-qlik-license.ps1 -LicenseKey "0123456789012345" -ControlNumber "01234" -LicenseeName "John Doe" -LicenseeOrg "Acme Corp"
+    Define all license details as parameters.
+#>
+
+
 param (
     [string] $LicenseKey     = $null,
     [string] $ControlNumber  = $null,
